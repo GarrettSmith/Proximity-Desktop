9a379518fae617ac9a1e48837fdd9b4706c79455
Garrett Smith <garrettwhsmith@gmail.com>
2012-07-23 15:52:06 -0500

    Improved about menu

--- src/ca/uwinnipeg/proximity/desktop/AboutDialog.java
+++ src/ca/uwinnipeg/proximity/desktop/AboutDialog.java
@@ -1,9 +1,17 @@
 package ca.uwinnipeg.proximity.desktop;
 
+import java.util.ResourceBundle;
+
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -11,9 +19,13 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class AboutDialog extends Dialog {
-  //private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
+  private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("ca.uwinnipeg.proximity.desktop.messages"); //$NON-NLS-1$
+  
+
+  private StackLayout stackLayout;
 
   /**
    * Create the dialog.
@@ -33,23 +45,48 @@ public class AboutDialog extends Dialog {
     container.setLayout(new GridLayout(1, false));
     
     Label lblIcon = new Label(container, SWT.NONE);
+    lblIcon.setImage(SWTResourceManager.getImage(AboutDialog.class, "/ca/uwinnipeg/proximity/desktop/icons/launcher_48.png"));
     lblIcon.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
     
     Label lblName = new Label(container, SWT.NONE);
-    lblName.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+    lblName.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+    lblName.setFont(SWTResourceManager.getFont("Sans", 10, SWT.BOLD));
     lblName.setText("Proximity System");
     
-    Label lblVersion = new Label(container, SWT.NONE);
+    Composite frameStack = new Composite(container, SWT.NONE);
+    stackLayout = new StackLayout();
+    frameStack.setLayout(stackLayout);
+    frameStack.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+        
+    Composite mainFrame = new Composite(frameStack, SWT.NONE);
+    mainFrame.setLayout(new GridLayout(1, false));
+    
+    Label lblVersion = new Label(mainFrame, SWT.NONE);
     lblVersion.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
-    lblVersion.setText("Version");
+    lblVersion.setText("1.0");
     
-    Label lblDescription = new Label(container, SWT.NONE);
+    Label lblDescription = new Label(mainFrame, SWT.NONE);
     lblDescription.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
-    lblDescription.setText("The Proxmity System desktop application.");
+    lblDescription.setText("The Proximity System desktop application.");
     
-    Label lblCopyright = new Label(container, SWT.NONE);
+    Label lblCopyright = new Label(mainFrame, SWT.NONE);
     lblCopyright.setLayoutData(new GridData(SWT.CENTER, SWT.BOTTOM, true, true, 1, 1));
+    lblCopyright.setFont(SWTResourceManager.getFont("Droid Sans", 8, SWT.NORMAL));
     lblCopyright.setText("© 2012 Holder");
+    
+    Composite licenceFrame = new Composite(frameStack, SWT.NONE);
+    licenceFrame.setLayout(new FillLayout(SWT.HORIZONTAL));
+    
+    StyledText licenceText = new StyledText(licenceFrame, SWT.BORDER);
+    licenceText.setEditable(false);
+    
+    Composite creditsFrame = new Composite(frameStack, SWT.NONE);
+    creditsFrame.setLayout(new FillLayout(SWT.HORIZONTAL));
+    
+    StyledText creditsText = new StyledText(creditsFrame, SWT.BORDER);
+    
+    stackLayout.topControl = mainFrame;
+    frameStack.layout();
 
     return container;
   }
@@ -67,21 +104,19 @@ public class AboutDialog extends Dialog {
         IDialogConstants.CLOSE_LABEL, 
         true);
     
-    Button btnCredits = createButton(
-        parent, 
-        IDialogConstants.CLOSE_ID, 
-        IDialogConstants.CLOSE_LABEL, 
-        false);
-    btnCredits.setEnabled(false);
-    btnCredits.setText("Credits");
-    
-    Button btnLicence = createButton(
-        parent, 
-        IDialogConstants.CLOSE_ID, 
-        IDialogConstants.CLOSE_LABEL, 
-        false);
-    btnLicence.setEnabled(false);
-    btnLicence.setText("Licence");
+//    Button btnCredits = createButton(
+//        parent, 
+//        IDialogConstants.CLOSE_ID, 
+//        IDialogConstants.CLOSE_LABEL, 
+//        false);
+//    btnCredits.setText("Credits");
+//    
+//    Button btnLicence = createButton(
+//        parent, 
+//        IDialogConstants.CLOSE_ID, 
+//        IDialogConstants.CLOSE_LABEL, 
+//        false);
+//    btnLicence.setText("Licence");
   }
 
   /**
@@ -99,5 +134,4 @@ public class AboutDialog extends Dialog {
         super.buttonPressed(buttonId);
     }
   }
-
 }
